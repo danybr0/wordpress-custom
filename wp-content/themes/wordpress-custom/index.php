cb47c8d91711a5b8d249da5076a72aff051dfc49
Leonardo Apolinário <leonardoapolinario@live.com>
2017-11-03 16:35:56 -0200

    Enable pagination

--- wp-content/themes/wordpress-custom/index.php
+++ wp-content/themes/wordpress-custom/index.php
@@ -10,6 +10,14 @@
             the_post(); 
             get_template_part('content', get_post_format()); 
           endwhile;
+          ?>
+          <nav>
+            <ul class="pager">
+              <li><?php next_posts_link( 'Anterior' ); ?></li>
+              <li><?php previous_posts_link( 'Próximo' ); ?></li>
+            </ul>
+          </nav>
+          <?php
         endif;
           
     ?>
