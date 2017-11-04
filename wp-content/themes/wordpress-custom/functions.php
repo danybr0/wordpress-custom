57f00456f1076900c092e27b9bfe86f6563e91dc
Leonardo Apolinário <leonardoapolinario@live.com>
2017-11-04 09:16:48 -0200

    Enqueue Google Fonts

--- wp-content/themes/wordpress-custom/functions.php
+++ wp-content/themes/wordpress-custom/functions.php
@@ -1,8 +1,17 @@
 <?php
+//Enqueue styles
 function wordpress_custom_styles() {
     wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7' );
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js' , array('jquery'), '3.3.7', true);
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_custom_styles' );
+
+// Add Google Fonts
+function startwordpress_google_fonts() {
+				wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+				wp_enqueue_style( 'OpenSans');
+		}
+
+add_action('wp_print_styles', 'startwordpress_google_fonts');
 ?>
