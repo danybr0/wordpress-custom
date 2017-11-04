1eb1438ca516c5a188fdc7301da0eab8339cd403
Leonardo Apolinário <leonardoapolinario@live.com>
2017-11-04 09:14:14 -0200

    Enqueue scripts and stylesheets

--- wp-content/themes/wordpress-custom/functions.php
+++ wp-content/themes/wordpress-custom/functions.php
@@ -1,14 +1,8 @@
 <?php
-function my_theme_enqueue_styles() {
-
-    $parent_style = 'parent-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+function wordpress_custom_styles() {
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js' , array('jquery'), '3.3.7', true);
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'wordpress_custom_styles' );
 ?>
--- wp-content/themes/wordpress-custom/header.php
+++ wp-content/themes/wordpress-custom/header.php
@@ -8,14 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title><?php echo get_bloginfo('name'); ?></title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
-
+  <title><?php echo get_bloginfo('name'); ?></title>  
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
